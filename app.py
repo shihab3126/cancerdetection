6fc2d323c96fa85d3551700524fbdc75a829513d
SHIHAB <149651672+shihab3126@users.noreply.github.com>
2025-08-22 06:26:03 +0600

    Update app.py

--- app.py
+++ app.py
@@ -104,4 +104,4 @@ def predict():
     return render_template('index.html', prediction_text=prediction_text)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
